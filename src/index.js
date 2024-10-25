c09cfd6b9cc135fb39925953503b44a3336f921b
sumit-demo <sumitsur200.sur@gmail.com>
2024-10-25 22:04:15 +0530

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,89 @@
+import dotenv from "dotenv";
+
+import connectDB from "./db/index.js";
+
+
+dotenv.config();
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+const app = express();
+
+(async () => {
+
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+
+        app.on("error"  , (error)=>{
+            console.log("Error : " , error);
+            throw error;
+        })
+
+        app.listen(process.env.PORT  ,  ()=>{
+
+            console.log(`App is listening on port ${process.env.PORT}`)
+        })
+
+    } catch (error) {
+        console.log("Error: ", error)
+        throw error
+    }
+
+})()
+*/
