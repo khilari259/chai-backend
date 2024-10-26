ba47052a7bda400648ebc10c263c358d0463e016
sumit-demo <sumitsur200.sur@gmail.com>
2024-10-26 20:27:08 +0530

    utils code

--- src/db/index.js
+++ src/db/index.js
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose';  
+import mongoose from 'mongoose';
 
 
 import { DB_NAME } from "../constants.js";
@@ -6,8 +6,8 @@ import { DB_NAME } from "../constants.js";
 const connectDB = async () => {
 
     try {
-   const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${ DB_NAME}`)
-   console.log(`MONGODB  connected !! DB HOST: ${connectionInstance.connection.host}`)
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        console.log(`MONGODB  connected !! DB HOST: ${connectionInstance.connection.host}`)
     } catch (error) {
         console.log("MONGODB  connection error : ", error);
         process.exit(1)
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,20 @@
+const asncHandler = (requestHandler) =>{
+     (req , res , next) => {
+        Promise.resolve(requestHandler(req , res , next))
+        .catch((err) => next(err))
+     }
+}
+
+export {asncHandler}
+
+
+// const asncHandler = (fn) => async (req , res , next)=>{
+//     try{
+//          await fn(req , res , next)
+//     } catch(error) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }
